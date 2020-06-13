dfd30b5c75696ad629600f0f4105f17590cbc8a1
Ruilong Xu <xuruilong100@163.com>
2020-06-13 22:46:33 +0800

    add script of exercise 5 of chapter 6

--- Chapter6.py
+++ Chapter6.py
@@ -3,7 +3,8 @@ import pandas as pd
 import seaborn as sb
 import statsmodels.api as sm
 from scipy import stats
-from arch.univariate import ConstantMean, StudentsT, ZeroMean
+from arch.univariate import ConstantMean, StudentsT, ZeroMean, GARCH
+from prettytable import PrettyTable
 from archEx.NGARCH import NGARCH11, FixedNGARCH11
 
 # Exercise 1
@@ -142,3 +143,77 @@ sb.distplot(rst.resid, fit=stats.t)
 
 sm.graphics.qqplot(
     rst.std_resid, line='45')
+
+# Exercise 5
+
+scale = 100.0
+
+spClose = pd.read_csv(
+    'data/Chapter4_Data1.csv', parse_dates=True,
+    index_col='Date', squeeze=True)
+
+returns = spClose.apply(np.log) - spClose.shift(1).apply(np.log)
+returns *= scale
+returns.dropna(inplace=True)
+
+# using GARCH(1,1)-t as filter
+
+tsm = ConstantMean(returns)
+garch = GARCH(p=1, q=1)
+tsm.volatility = garch
+tsm.distribution = StudentsT()
+rst = tsm.fit()
+
+print(rst)
+
+sb.distplot(rst.resid, fit=stats.t)
+
+sm.graphics.qqplot(
+    rst.std_resid, line='45')
+
+
+def HillEstimator(resid, num):
+    sortedResid = np.sort(resid)
+    tailLoss = -sortedResid[0:num]
+    u = -sortedResid[num + 1]
+
+    xi = np.mean(np.log(tailLoss)) - np.log(u)
+    c = num / len(resid) * u ** (1.0 / xi)
+
+    est = {'xi': xi, 'c': c, 'u': u}
+    return est
+
+
+hillEst = HillEstimator(rst.resid, 50)
+print(hillEst)
+
+
+def HillPpf(q, hill_est):
+    return 1.0 / (q / hill_est['c']) ** hill_est['xi']
+
+
+def CornishFisherPpf(q, resid):
+    nppf = stats.norm.ppf(q)
+    s = stats.skew(resid)
+    k = stats.kurtosis(resid)
+
+    cfp = -(nppf + 0.74 * s - 0.24 * k + 0.38 * s ** 2)
+
+    return cfp
+
+
+tailProb = 1 / 100.0
+
+nu = rst.params['nu']
+
+tab = PrettyTable(['Model', 'Tail Quantile'])
+tab.add_row(['normal', -stats.norm.ppf(tailProb)])
+tab.add_row(['standardized t', -StudentsT().ppf(tailProb, nu)])
+# tab.add_row([
+#     'standardized t',
+#     stats.t.ppf(
+#         tailProb, df=nu, scale=1 / stats.t.std(df=nu))])
+tab.add_row(['Hill', HillPpf(tailProb, hillEst)])
+tab.add_row(['Cornish-Fisher', CornishFisherPpf(tailProb, rst.resid)])
+
+print(tab)
